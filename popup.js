1362bc23fb6ca517b4e761f8ffadfb7ce19758d1
cyhboy <cyhboy@tom.com>
2022-10-11 23:56:36 +0800

    align same name for html and js file. popup module is useless, just keep for further usage

--- popup.js
+++ popup.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-undef */
-
 
 
 if (typeof window !== 'undefined') {
@@ -9,68 +7,4 @@ if (typeof window !== 'undefined') {
     console.log('You are on the server in popup.js')
     // can't use localStorage
 }
-
-
-chrome.storage.local.get(['domContent'], function (result) {
-    console.log('Value currently is ' + result.domContent)
-
-    let myFile = 'learn/' + result.domContent + '.xlsm'
-
-    console.log(myFile)
-
-    chrome.runtime.getPackageDirectoryEntry(function (storageRootEntry) {
-        storageRootEntry.getFile(myFile, {
-        }, function (fileEntry) {
-            fileEntry.file(function (file) {
-                let output = 'ng'
-                let color = 'red'
-                let dtitle = 'yt index files is not available'
-                console.log('file.name: ', file.name)
-                console.log('modification: ', file.lastModifiedDate)
-                let date1 = new Date(file.lastModified)
-                let date2 = new Date()
-                let diffTime = Math.abs(date2 - date1)
-                let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
-                console.log(diffTime + ' milliseconds')
-                console.log(diffDays + ' days')
-                if (diffDays > 180) {
-                    output = 'ok'
-                    color = 'orange'
-                    dtitle = 'yt index files outdated over half year'
-                } else if (diffDays > 90) {
-                    output = 'ok'
-                    color = 'yellow'
-                    dtitle = 'yt index files outdated over 3 months'
-                } else if (diffDays > 30) {
-                    output = 'ok'
-                    color = 'blue'
-                    dtitle = 'yt index file is pending for review'
-                } else {
-                    output = 'ok'
-                    color = 'green'
-                    dtitle = 'yt index file just review in passed month'
-                }
-
-                chrome.storage.local.set({ 'output': output, 'color': color, 'dtitle': dtitle }, function () {
-                    console.log('output / color / dtitle is set again', output, color, dtitle)
-                    window.close()
-                })
-
-            })
-        }, function () {
-            let output = 'ng'
-            let color = 'red'
-            let dtitle = 'yt index files is not available'
-            chrome.storage.local.set({ 'output': output, 'color': color, 'dtitle': dtitle }, function () {
-                console.log('output / color / dtitle is set again', output, color, dtitle)
-                window.close()
-            })
-        })
-
-
-    })
-
-
-})
-
 
